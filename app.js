bfa990c857d91a8d5b9d7a96d5b4b6bcdcd96077
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-02 22:29:51 -0400

    fourth commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const input = document.querySelector("#zip")
 const button = document.querySelector("button")
-const searchBox = document.querySelector("#restmov")
+const searchBox = document.querySelector(".restmov")
 
 
 button.addEventListener("click", async () => {
@@ -16,12 +16,13 @@ button.addEventListener("click", async () => {
 
   restaurants.map(restaurant => {
     let searchRes = document.createElement('div');
-    searchRes.className = 'card';
+    searchRes.className = "flip-inner";
     // display.innerHTML += `<p>Name: ${restaurant.name}</p><p>Grade: ${restaurant.grade}</p><p>Violation: ${restaurant.description}</p>`
-    searchRes.innerHTML += `<h2>${restaurant.name}</h2> <h3 class="codeRed">${restaurant.grade}</h3> <p>What's Gross: ${restaurant.description}</p>`
+    searchRes.innerHTML += `<div class="flip-front"><h2>${restaurant.name}</h2></div> <div class="flip-back"><h3 class="codeRed">
+    ${restaurant.grade}</h3> <p>What's Gross: ${restaurant.description}</p></div>`
     searchBox.append(searchRes)
 
-    document.querySelector('#restmov').scrollIntoView({ behavior: 'smooth' })
+    document.querySelector('.restmov').scrollIntoView({ behavior: 'smooth' })
   })
 
 
