76c1a008d2ca982a2ef04a522aca86927ca5dc4d
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-03 22:50:49 -0400

    Final v.1

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const pageTwo = document.querySelector("#page-two")
 
 button.addEventListener("click", async () => {
   let name = input.value
-  display.innerHTML=" "
+  display.innerHTML = " "
   const response = await axios.get(`https://data.cityofnewyork.us/resource/43nn-pn8j.json?grade=C&zipcode=${name}`)
 
   let restaurants = response.data.map(restaurant => {
-    return { name: restaurant.dba, grade: restaurant.grade, description: restaurant.violation_description, building: restaurant.building, street: restaurant.street  }
-  }); 
+    return { name: restaurant.dba, grade: restaurant.grade, description: restaurant.violation_description, building: restaurant.building, street: restaurant.street }
+  });
 
   console.log(response)
   console.log(restaurants)
@@ -20,22 +20,13 @@ button.addEventListener("click", async () => {
   restaurants.map(restaurant => {
     let searchRes = document.createElement('div');
     searchRes.className += "flip-inner";
-    // searchBox.innerHTML += `<div class="flip-front"><h2 id="restname">${restaurant.name}</h2></div> 
-    // <div class="flip-back"><h3 class="codeRed">Offense:</h3> <p>${restaurant.description}</p></div>`
     searchRes.innerHTML += `<div class="flip-front"><h2 id="restname">${restaurant.name}</h2><p id="streetAdd">${restaurant.building} <br/> ${restaurant.street}</div> <div class="flip-back"><h3 class="codeRed">Offense:</h3><p id="desc"> ${restaurant.description}</p></div>`
     display.append(searchRes)
 
     pageOne.style.display = "none"
     pageTwo.style.display = "block"
-    
-  })
-  
-  // let card = document.querySelector(".flip-inner")
-  // card.addEventListener('click', async () => {
-  //   card.classList.toggle('flippy');
-    
 
-  // })
+  })
 
 
 })
