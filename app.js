c61884cbcf31ceaacbfcddde83bfd08d6f1622e5
slvteblvvk <43097045+slvteblvvk@users.noreply.github.com>
2019-10-04 12:26:28 -0400

    Final V3

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const button = document.querySelector("button")
 const display = document.querySelector(".restmov")
 const pageOne = document.querySelector("#page-one")
 const pageTwo = document.querySelector("#page-two")
+// Testing out new page//
+const pageThree = document.querySelector("#page-three")
+const inputPlace = document.querySelector("#business")
+const press = document.querySelector("#action")
+const showMe = document.querySelector(".findresta")
 
 button.addEventListener("click", async () => {
   let name = input.value
@@ -25,6 +30,38 @@ button.addEventListener("click", async () => {
 
     pageOne.style.display = "none"
     pageTwo.style.display = "block"
+
+  })
+
+
+})
+
+// Testing out new page results //
+
+press.addEventListener("click", async () => {
+  let nameTwo = inputPlace.value.toUpperCase()
+  showMe.innerHTML = " "
+  const answer = await axios.get(`https://data.cityofnewyork.us/resource/43nn-pn8j.json?dba=${nameTwo}`)
+
+  let manyRest = answer.data.map(foodres => {
+    return { name: foodres.dba, description: foodres.violation_description, building: foodres.building, street: foodres.street }
+  });
+
+  console.log(answer)
+  console.log(manyRest)
+
+
+  manyRest.map(foodres => {
+
+    let yourSearch = document.createElement('div');
+    yourSearch.className += "card-container";
+    yourSearch.innerHTML += `<h2 id="restnameTwo">${foodres.name}</h2> 
+    <p id="streetAddTwo">${foodres.building}<br/>${foodres.street} 
+    <p id="descTwo">${foodres.description}</p>`
+    showMe.append(yourSearch)
+
+    pageOne.style.display = "none"
+    pageThree.style.display = "block"
 
   })
 
